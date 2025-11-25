9dcb793817ae0496682d40a5871e82f6e5a8192d
ramkrish0403 <ramkrish0403@gmail.com>
2025-11-25 05:40:25 +0000

    init

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,9 @@
+git config --global --add safe.directory /workspaces/python-starter
+
+cd /workspaces/python-starter
+# pip install uv
+# echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
+
+# uv self update
+uv sync
+# uv install
